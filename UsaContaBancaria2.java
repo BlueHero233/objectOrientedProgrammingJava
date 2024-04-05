4cbad7bb11c365d56b68f8b28835d54f14d2b74f
BlueHero233 <87818077+BlueHero233@users.noreply.github.com>
2024-04-04 23:00:49 -0300

    Add files via upload

---
+++ UsaContaBancaria2.java
@@ -0,0 +1,11 @@
+public class UsaContaBancaria2 {
+    public static void main(String[] args){
+    ContaBancaria2 fred = new ContaBancaria2("Fred" ,1000);
+    ContaBancaria2 richard = new ContaBancaria2("Richard" ,2000);
+    System.out.println(fred);
+    System.out.println(richard);
+    fred.transfereDe(richard ,400); //e se fosse 3.000?
+    System.out.println(fred);
+    System.out.println(richard);
+    }
+}
